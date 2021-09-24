d438d362a0d5b9d5573ed7915d44721921d7e831
wjdrmsl <wjdrmsl@gmail.com>
2021-09-24 16:28:47 +0900

    사이드 메뉴

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,78 @@
 <template>
   <div class="flex h-screen">
     <!-- 왼쪽ㄹ -->
-    <div class="w-1/4 mt-5 ml-10">
-      <!-- twitter icon -->
-      <i class="fab fa-twitter text-2xl text-primary ml-4"></i>
-      <!-- sidemenu -->
-      <div class="flex flex-col items-start">
-        <!-- sidemenu items -->
-        <div class="hover:text-primary hover:bg-lighter px-4 py-2 rounded-full">
-          <i class="fas fa-home fa-fw text-2xl"></i>
-          <span class="ml-5 text-lg">홈</span>
+    <div class="w-1/4 mt-5 ml-10 flex flex-col justify-between">
+      <div>
+        <!-- twitter logo -->
+        <i class="fab fa-twitter text-2xl text-primary ml-4"></i>
+        <!-- sidemenu -->
+        <div class="flex flex-col items-start space-y-1">
+          <!-- sidemenu items -->
+          <div
+            class="hover:text-primary hover:bg-lighter px-4 py-2 rounded-full"
+          >
+            <i class="fas fa-home fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg">홈</span>
+          </div>
+          <div
+            class="hover:text-primary hover:bg-lighter px-4 py-2 rounded-full"
+          >
+            <i class="fas fa-hashtag fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg">탐색하기</span>
+          </div>
+          <div
+            class="hover:text-primary hover:bg-lighter px-4 py-2 rounded-full"
+          >
+            <i class="fas fa-bell fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg">알림</span>
+          </div>
+          <div
+            class="hover:text-primary hover:bg-lighter px-4 py-2 rounded-full"
+          >
+            <i class="fas fa-envelope fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg">쪽지</span>
+          </div>
+          <div
+            class="hover:text-primary hover:bg-lighter px-4 py-2 rounded-full"
+          >
+            <i class="fas fa-bookmark fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg">북마크</span>
+          </div>
+          <div
+            class="hover:text-primary hover:bg-lighter px-4 py-2 rounded-full"
+          >
+            <i class="fas fa-list-alt fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg">리스트</span>
+          </div>
+          <div
+            class="hover:text-primary hover:bg-lighter px-4 py-2 rounded-full"
+          >
+            <i class="fas fa-user fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg">프로필</span>
+          </div>
+          <div
+            class="hover:text-primary hover:bg-lighter px-4 py-2 rounded-full"
+          >
+            <i class="fas fa-ellipsis-h fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg">더보기</span>
+          </div>
         </div>
-        <div class="hover:text-primary hover:bg-lighter px-4 py-2 rounded-full">
-          <i class="fas fa-hashtag fa-fw text-2xl"></i>
-          <span class="ml-5 text-lg">탐색하기</span>
+        <!-- tweet button -->
+        <div class="mr-3">
+          <button
+            class="
+              mt-3
+              bg-primary
+              text-white
+              w-full
+              h-12
+              rounded-full
+              hover:bg-dark
+            "
+          >
+            Tweet
+          </button>
         </div>
-        <div class="hover:text-primary hover:bg-lighter px-4 py-2 rounded-full">
-          <i class="fas fa-bell fa-fw text-2xl"></i>
-          <span class="ml-5 text-lg">알림</span>
-        </div>
-        <div class="hover:text-primary hover:bg-lighter px-4 py-2 rounded-full">
-          <i class="fas fa-envelope fa-fw text-2xl"></i>
-          <span class="ml-5 text-lg">쪽지</span>
-        </div>
-        <div class="hover:text-primary hover:bg-lighter px-4 py-2 rounded-full">
-          <i class="fas fa-bookmark fa-fw text-2xl"></i>
-          <span class="ml-5 text-lg">북마크</span>
-        </div>
-        <div class="hover:text-primary hover:bg-lighter px-4 py-2 rounded-full">
-          <i class="fas fa-list-alt fa-fw text-2xl"></i>
-          <span class="ml-5 text-lg">리스트</span>
-        </div>
-        <div class="hover:text-primary hover:bg-lighter px-4 py-2 rounded-full">
-          <i class="fas fa-user fa-fw text-2xl"></i>
-          <span class="ml-5 text-lg">프로필</span>
-        </div>
-        <div class="hover:text-primary hover:bg-lighter px-4 py-2 rounded-full">
-          <i class="fas fa-ellipsis-h fa-fw text-2xl"></i>
-          <span class="ml-5 text-lg">더보기</span>
-        </div>
-      </div>
-      <!-- tweet button -->
-      <div class="mr-3">
-        <button
-          class="
-            mt-3
-            bg-primary
-            text-white
-            w-full
-            h-12
-            rounded-full
-            hover:bg-dark
-          "
-        >
-          Tweet
-        </button>
       </div>
       <!-- profile button -->
       <div class="mr-3 mb-3">
